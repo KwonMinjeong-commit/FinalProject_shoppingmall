289f9ece2d6f31ea1af93fff247409f2e1e1160d
KwonMinjeong-commit <mxnjxxng@naver.com>
2022-12-18 02:15:09 +0900

    카테고리 페이지, 제조사 페이지 만들기

--- product/urls.py
+++ product/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('category/<str:slug>/', views.category_page),  # 카테고리 페이지
+    path('manufacturer/<str:slug>/', views.manufacturer_page),  # 제조사 페이지
     path('', views.ProductList.as_view()),  # 상품 목록 페이지
     path('<str:slug>/', views.ProductDetail.as_view()), # 상품 상세 페이지
-
 ]
--- product/views.py
+++ product/views.py
@@ -21,3 +21,25 @@ class ProductDetail(DetailView):
         context['categories'] = Category.objects.all()      # 카테고리의 모든 정보 전달
         context['no_category_post_count'] = Product.objects.filter(category=None).count
         return context
+
+def category_page(request, slug):
+    category = Category.objects.get(slug=slug)
+    product_list = Product.objects.filter(category=category)
+
+    return render(request, 'product/product_list.html', {
+        'category': category,  # 'category': 템플릿 안의 변수명
+        'product_list': product_list,
+        'categories': Category.objects.all(),  # 사이드바 정상적 출력을 위해
+        'no_category_post_count': Product.objects.filter(category=None).count  # 사이드바 정상적 출력을 위해
+    })
+
+def manufacturer_page(request, slug):
+    manufacturer = Manufacturer.objects.get(slug=slug)
+    product_list = Product.objects.filter(manufacturer=manufacturer)
+
+    return render(request, 'product/product_list.html', {
+        'manufacturer': manufacturer,  # 'category': 템플릿 안의 변수명
+        'product_list': product_list,
+        'categories': Category.objects.all(),  # 사이드바 정상적 출력을 위해
+        'no_category_post_count': Product.objects.filter(category=None).count  # 사이드바 정상적 출력을 위해
+    })
