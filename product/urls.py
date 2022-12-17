cf4dc2436591fb190fa22b6aa2a9f4917c2196c5
KwonMinjeong-commit <mxnjxxng@naver.com>
2022-12-18 02:47:24 +0900

    태그 페이지 만들기

--- product/urls.py
+++ product/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('color/<str:slug>/', views.color_page),  # 색 페이지
+    path('type/<str:slug>/', views.type_page),  # 기종 페이지
     path('category/<str:slug>/', views.category_page),  # 카테고리 페이지
     path('manufacturer/<str:slug>/', views.manufacturer_page),  # 제조사 페이지
     path('', views.ProductList.as_view()),  # 상품 목록 페이지
--- product/views.py
+++ product/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Product, Category, Manufacturer
+from .models import Product, Category, Manufacturer, Color, Type
 
 # Create your views here.
 class ProductList(ListView):
@@ -40,6 +40,26 @@ def manufacturer_page(request, slug):
     return render(request, 'product/product_list.html', {
         'manufacturer': manufacturer,  # 'category': 템플릿 안의 변수명
         'product_list': product_list,
+        'categories': Category.objects.all(),  # 사이드바 정상적 출력을 위해
+        'no_category_post_count': Product.objects.filter(category=None).count  # 사이드바 정상적 출력을 위해
+    })
+
+def color_page(request, slug):
+    color = Color.objects.get(slug=slug)
+    product_list = color.product_set.all()
+    return render(request, 'product/product_list.html', {
+        'color' : color,
+        'product_list' : product_list,
+        'categories': Category.objects.all(),  # 사이드바 정상적 출력을 위해
+        'no_category_post_count': Product.objects.filter(category=None).count  # 사이드바 정상적 출력을 위해
+    })
+
+def type_page(request, slug):
+    type = Type.objects.get(slug=slug)
+    product_list = type.product_set.all()
+    return render(request, 'product/product_list.html', {
+        'type' : type,
+        'product_list' : product_list,
         'categories': Category.objects.all(),  # 사이드바 정상적 출력을 위해
         'no_category_post_count': Product.objects.filter(category=None).count  # 사이드바 정상적 출력을 위해
     })
